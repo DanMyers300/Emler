962b87b737fb7e9f09b47213d1f024d6b75f50e9
Dan Myers <106322207+Colloke@users.noreply.github.com>
2023-01-02 19:00:33 -0600

    Update Eval_List.py

--- Eval_List.py
+++ Eval_List.py
@@ -1,1 +1,10 @@
-
+import selenium
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+import time
+import pyautogui
+import pyperclip as pc
+import pygsheets
+import pandas as pd
+import datetime
+from datetime import datetime, timedelta
