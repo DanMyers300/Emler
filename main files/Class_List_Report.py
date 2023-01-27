5fe5f485ff53d7e454b0cc8841543bd32682ec3c
Dan Myers <106322207+Colloke@users.noreply.github.com>
2023-01-26 18:49:54 -0600

    Update Class_List_Report.py

--- main files/Class_List_Report.py
+++ main files/Class_List_Report.py
@@ -1,29 +1,13 @@
 import selenium
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import time
-import pyautogui
-import pyperclip as pc
-import pygsheets
-import pandas as pd
-import datetime
-from datetime import datetime, timedelta
 
-
-# Authorize pygsheets using the JSON file
-gc = pygsheets.authorize(service_file='python-368100-5f317bcc7d43.json')
-
-# Get the current date
-current_date = datetime.now()
-
-# Calculate the date two weeks from the current date
-two_weeks_from_now = current_date + timedelta(weeks=2)
-
-#screensize for autogui
-screenWidth, screenHeight = pyautogui.size()
-
-#Login to ICP
 driver = webdriver.Chrome()
+
+#Load ICP
 driver.get('https://app.iclasspro.com/a/emlerdallas')
 id_box = driver.find_element('id', 'uname')
 id_box.send_keys('dmyerspf')
@@ -32,34 +16,17 @@ pass_box.send_keys('Emler123')
 login_button = driver.find_element('id', 'loginbutton')
 login_button.click()
 
-#Run class list report
-driver.get('https://app.iclasspro.com/a/emlerdallas/#!/report/classes/1')
+#Open up the report and run it
+driver.get('https://app.iclasspro.com/a/emlerdallas#!/report/classes/4')
 time.sleep(2)
-pyautogui.click(500,550)
-time.sleep(1)
-pyautogui.click(500,580)
-date_picker = driver.find_element('name', 'endDate')
-date_picker.click()
-pyautogui.write(two_weeks_from_now.strftime("%m/%d/%Y"), interval=0.25)
-pyautogui.press('esc')
-pyautogui.moveTo(400, 300)
-pyautogui.drag(0, 400, 2, button='middle')
-pyautogui.click(300,900)
-time.sleep(20)
-pyautogui.click(700,430)
-
-#Copy everything
-pyautogui.hotkey('ctrl', 'a', interval=0.25)
-pyautogui.hotkey('ctrl', 'c', interval=0.25)
-
-#Add copied information into a dataframe
-df = pd.read_clipboard(header=None)
-
-#And paste into google sheets
-destination_sheet = gc.open("PF Office Hub - Fall 2022")
-destination_worksheet = destination_sheet.worksheet_by_title("Class List Report")
-destination_worksheet.update_values('K1', df)
-
-#wait so screen stays up for 10 seconds
-print('Success!')
-time.sleep(10)
+filter_list = driver.find_element('id', 'filter-preset-list')
+filter_list.click()
+filter_list.send_keys(Keys.ARROW_DOWN * 2)
+filter_list.send_keys(Keys.ENTER)
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+time.sleep(2)
+html_button = driver.find_element(By.XPATH, "//*[@id='appdata-container']/icp-single-page-report-filters/div/div[2]/div/div[2]/div/div[3]/div[1]/button")
+html_button.click()
+time.sleep(15)
+driver.find_element(By.XPATH, "//*[@id='icp-modal-layout-1674780145655']/ng-transclude/table/tbody/tr[2]/td[2]/button[1]")
+time.sleep(1000)
