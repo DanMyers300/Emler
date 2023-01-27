054fed95e876a7e2466f10358b028e09d4ada727
Dan Myers <106322207+Colloke@users.noreply.github.com>
2023-01-26 18:53:50 -0600

    Update Class_List_Report.py

--- main files/Class_List_Report.py
+++ main files/Class_List_Report.py
@@ -4,6 +4,7 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
+import pyautogui
 
 driver = webdriver.Chrome()
 
