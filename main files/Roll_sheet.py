5e11460c5682e78e264bcc72b0e498e0e88828b7
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2023-02-02 18:05:59 -0600

    Update Roll_sheet.py

--- main files/Roll_sheet.py
+++ main files/Roll_sheet.py
@@ -1,15 +1,14 @@
 import selenium
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import time
 import pyautogui
-import pygsheets
 
-#Authorize
-gc = pygsheets.authorize(service_file='python-368100-5f317bcc7d43.json')
-
-#Login to ICP
 driver = webdriver.Chrome()
+
+#Load ICP
 driver.get('https://app.iclasspro.com/a/emlerdallas')
 id_box = driver.find_element('id', 'uname')
 id_box.send_keys('dmyerspf')
@@ -17,29 +16,32 @@ pass_box = driver.find_element('id', 'passwd')
 pass_box.send_keys('Emler123')
 login_button = driver.find_element('id', 'loginbutton')
 login_button.click()
+
+#Open up the report and run it
 driver.get('https://app.iclasspro.com/a/emlerdallas#!/report/classes/4')
-
-#Open the print dialog
+time.sleep(2)
+filter_list = driver.find_element('id', 'filter-preset-list')
+filter_list.click()
+filter_list.send_keys(Keys.ARROW_DOWN * 2)
+filter_list.send_keys(Keys.ENTER)
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+time.sleep(2)
+html_button = driver.find_element(By.XPATH, "//*[@id='appdata-container']/icp-single-page-report-filters/div/div[2]/div/div[2]/div/div[3]/div[1]/button")
+html_button.click()
+time.sleep(20)
+pyautogui.click(730,420)
 time.sleep(5)
-pyautogui.click(400,550)
-pyautogui.click(400,620)
+pyautogui.keyDown('ctrl')
+pyautogui.press('s')
+pyautogui.keyUp('ctrl')
 time.sleep(2)
-pyautogui.drag(0, 400, 2, button='middle')
-pyautogui.click(400,900)
-time.sleep(30)
-pyautogui.click(720,420)
-time.sleep(2)
-
-#Send Print information
-pyautogui.hotkey('ctrl', 'p', interval=0.25)
-time.sleep(5)
-pyautogui.click(800,450)
-time.sleep(2)
-pyautogui.drag(0, 400, 2, button='middle')
-
-#This uses the system dialog instead of chrome print 
-pyautogui.hotkey('ctrl', 'shift', 'p', interval=0.25)
-pyautogui.click(280,420)
-
-#Wait so the dialog stays open
+pyautogui.press('enter', presses=2)
 time.sleep(10)
+'''
+To-Do:
+
+        1) Remove time.sleep(s) as it is bad practice
+        2) Currently just saves the file, write script to print it using defualt printer
+        3) Remove pyautogui as it is bad practice
+
+'''
