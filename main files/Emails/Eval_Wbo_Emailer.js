c7668c6e266858fd5f8ea5c531b2a2ceee744cff
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2023-02-23 17:39:12 -0600

    Update Eval_Wbo_Emailer.js

--- main files/Emails/Eval_Wbo_Emailer.js
+++ main files/Emails/Eval_Wbo_Emailer.js
@@ -1,31 +1,31 @@
+// This function is currently failing because m, d, and y are returning NAN. It seems like it's a parsing error from google sheets
+
 function sendEmail() {
   const sheet = SpreadsheetApp.getActive().getSheetByName('WBO/Eval List');
   const dataRange = sheet.getDataRange();
   const values = dataRange.getValues();
   const lastRow = dataRange.getLastRow();
 
-  let today = new Date();
-  let y0 = today.getFullYear();
-  let m0 = today.getMonth() + 1;
-  let d0 = today.getDate();
-
-// This is currently failing because of JS NAN error
-
   for (let i = 2; i < lastRow+1; i++) {
+    let today = new Date();
+    let y0 = today.getFullYear();
+    let m0 = today.getMonth() + 1;
+    let d0 = today.getDate();
+    
     x = Date.parse(values[i]);
     const date = new Date(x);
-    const y = date.getFullYear();
-    const m = date.getMonth() + 1;
-    const d = date.getDate();
-
+    let y = date.getFullYear();
+    let m = date.getMonth() + 1;
+    let d = date.getDate();
+    
     if(y0 === y && m0 === m && d0 === d) {
     const emailAddress = `contact@danmyers.net`; // Read email in column J
     const message = "Hello world!";
     const subject = "Hello world!";
 
-    // console.log(emailAddress, message, subject);
+    console.log(emailAddress, message, subject);
     //MailApp.sendEmail(emailAddress, subject, message);
 
-    }
+    } console.log(m,d,y,y0,m0,d0)
   }
 }
