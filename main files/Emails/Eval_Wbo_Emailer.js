ff95417e482b10421d109bdabaee802778b91585
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2023-02-26 11:06:02 -0600

    Update Eval_Wbo_Emailer.js

--- main files/Emails/Eval_Wbo_Emailer.js
+++ main files/Emails/Eval_Wbo_Emailer.js
@@ -1,22 +1,27 @@
 function sendEmails() {
-  var spreadsheet = SpreadsheetApp.openById("1VOrghqm-GxEsdgcuXBHt8neC8mTBtlhqiSsy-VyWKCU");
-  var sheet = spreadsheet.getSheetByName("WBO/Eval List");
-  var startRow = 2; // Start at second row to skip header row
-  var numRows = sheet.getLastRow() - 1; // Number of rows to process
-  
-  // Iterate through rows
+  const ss = SpreadsheetApp.getActiveSpreadsheet();
+  const sheet = ss.getSheetByName(`WBO/Eval List`);
+
+  const startRow = 2; // Start at second row to skip header row
+  const numRows = sheet.getLastRow() - 1; // Number of rows to process
+
   for (var i = 0; i < numRows; i++) {
     var row = sheet.getRange(startRow + i, 1, 1, sheet.getLastColumn()).getValues()[0];
-    var dateValue = row[1];
+    var dateValue = row[2];
     var email = row[9];
-    
-    // Check if date is later than today's date
+
+    // Skip row if email is empty
+    if (!email) {
+      continue;
+    }
+
     if (dateValue instanceof Date && dateValue.getTime() > new Date().getTime()) {
       // Send email
       var subject = "Reminder: Upcoming Event";
       var body = "This is a reminder that the event is coming up soon.";
-      MailApp.sendEmail(email, subject, body);
-      console.log(dateValue);
+      //MailApp.sendEmail(email, subject, body);
+      console.log(row);
     }
   }
+  console.log("end of data");
 }
