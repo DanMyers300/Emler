ec825ffcb778451eaf227a08e9f1064bcf3fa9d7
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2023-02-26 11:39:32 -0600

    Update Eval_Wbo_Emailer.js
    Finally works as expected. Sends just a bland email at the moment

--- main files/Emails/Eval_Wbo_Emailer.js
+++ main files/Emails/Eval_Wbo_Emailer.js
@@ -1,4 +1,4 @@
-function sendEmails() {
+function sendEvaluationWaterBabyOrientationEmails() {
   var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('WBO/Eval List').getDataRange().getValues();
   
   for (var i = 2; i < sheet.length; i++) {
